aa4bd3ddb9a10e9355e11408df3c3dff0f310a8c
João Vítor <joaovsoutosantos@gmail.com>
2021-09-10 19:37:52 -0300

    Control pagination button states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,9 +49,13 @@ export default function Home(): JSX.Element {
     [data]
   );
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -59,7 +63,12 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
